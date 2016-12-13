5698f316bcb86720394fa8f93028b9cc00c39a8c
gagner2 <gagner2@yahoo.com>
2016-12-12 22:57:26 -0500

    Update translate.py
    changed parameters

--- translator/translate.py
+++ translator/translate.py
@@ -1,4 +1,5 @@
-"""Binary for training translation models and decoding from them.
+"""
+Binary for training translation models and decoding from them.
 
 Running this program without --decode will download the WMT corpus into
 the directory specified as --data_dir and tokenize it in a very basic way,
@@ -7,10 +8,6 @@ and then start training a model saving checkpoints to --train_dir.
 Running with --decode starts an interactive loop so you can see how
 the current checkpoint translates English sentences into French.
 
-See the following papers for more information on neural translation models.
- * http://arxiv.org/abs/1409.3215
- * http://arxiv.org/abs/1409.0473
- * http://arxiv.org/abs/1412.2007
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -30,7 +27,7 @@ from tensorflow.models.rnn.translate import data_utils
 from tensorflow.models.rnn.translate import seq2seq_model
 
 
-tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate", 0.01, "Learning rate.")
 tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
                           "Learning rate decays by this much.")
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
@@ -45,7 +42,7 @@ tf.app.flags.DEFINE_string("data_dir", "/tmp", "Data directory")
 tf.app.flags.DEFINE_string("train_dir", "/tmp", "Training directory.")
 tf.app.flags.DEFINE_integer("max_train_data_size", 0,
                             "Limit on the size of training data (0: no limit).")
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 50,
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
                             "How many training steps to do per checkpoint.")
 tf.app.flags.DEFINE_boolean("decode", False,
                             "Set to True for interactive decoding.")
@@ -57,7 +54,6 @@ tf.app.flags.DEFINE_boolean("use_fp16", False,
 FLAGS = tf.app.flags.FLAGS
 
 # We use a number of buckets and pad to the closest one for efficiency.
-# See seq2seq_model.Seq2SeqModel for details of how they work.
 _buckets = [(5, 10), (10, 15), (20, 25), (40, 50)]
 
 
